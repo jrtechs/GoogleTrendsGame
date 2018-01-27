3c2cbec0a3df182cf81923957a5dc6be378fefb1
Jeffery R <jrtechs@hotmail.com>
2018-01-27 11:52:24 -0500

    Created classes in server for room and player

--- googletrendsgame/server/server.js
+++ googletrendsgame/server/server.js
@@ -4,9 +4,63 @@ var io = require('socket.io')(http);
 
 const port = 3000;
 
+var room = function(capacityP, roomN)
+{
+    //max capacity of room -- default is 4 for now
+    this.capacity = capacityP;
+
+    //name of the room
+    this.roomName = roomN;
+
+    //list of words used in the game
+    this.words = [];
+
+    //list of clients sockets -- so we can push requests to them
+    this.users = [];
+
+    //increments when rounds pass
+    this.currentRoom = 0;
+
+    /**
+     * adds a user to a room
+     * @param socket
+     */
+    this.addUser = function(socket)
+    {
+
+    }
+
+}
+
+var player = function(name)
+{
+    //name of the user
+    this.name = name;
+
+    //score of the player
+    this.score = 0;
+
+    //reference to the room -- might not need this
+    this.room = null;
+
+
+    //
+    this.sumbission = null;
+}
+
+//list of all the rooms
+var rooms = [];
+
 //Whenever someone connects this gets executed
-io.on('connection', function(socket) {
+io.on('connection', function(socket)
+{
     console.log('A user connected');
+
+
+    socket.on('clientEvent', function(data) {
+        console.log(data);
+    });
+
 
     //Whenever someone disconnects this piece of code executed
     socket.on('disconnect', function () {
---
+++ googletrendsgame/server/utils.js
@@ -0,0 +1,16 @@
+
+var words = [];
+
+
+
+module.exports=
+    {
+        /**
+         * Returns a random word
+         * @returns {string}
+         */
+        getRandomWord : function()
+        {
+            return '';
+        }
+    };
