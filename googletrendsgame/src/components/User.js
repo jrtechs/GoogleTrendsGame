33cac9190a4fd4a23d551e42ca58e4d207dd6c74
Unknown <jpramassini@gmail.com>
2018-01-28 01:01:11 -0500

    Auto stash before merge of "master" and "origin/master"

--- googletrendsgame/src/components/User.js
+++ googletrendsgame/src/components/User.js
@@ -1,3 +1,32 @@
 import React from 'react';
+import FontAwesome from 'react-fontawesome';
 
-function
+export default (props) => {
+  const name = this.props.name;
+  const score = this.props.score;
+  var word;
+  if (this.props.word === ''){
+    word = 'true'
+  } else {
+    word = 'false'
+  }
+
+
+    if (word === 'true') {
+      return(
+      <li>
+        <div>{name}</div>
+        <div>{score}</div>
+        <FontAwesome name='check-circle' />
+      </li>
+    )
+  } else {
+    return (
+      <li>
+        <div>Name: {name}</div>
+        <div>Score: {score}</div>
+        <FontAwesome name='times-circle' />
+      </li>
+    )
+  }
+}
