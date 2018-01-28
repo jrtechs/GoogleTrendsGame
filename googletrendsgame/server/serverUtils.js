97b8d35b95e641454f8fee9f5286fc02fc538956
Jeffery R <jrtechs@hotmail.com>
2018-01-27 23:12:51 -0500

    updated roomUpdate generate json

--- googletrendsgame/server/serverUtils.js
+++ googletrendsgame/server/serverUtils.js
@@ -1,39 +1,9 @@
 module.exports=
     {
-        /**
-         * Returns a random word
-         * @returns {string}
-         */
-        roomOpen : function(name, rooms)
-        {
-            rooms.foreach(function(r)
-            {
-                if(name === r.roomName)
-                {
-                    return false;
-                }
-            });
-            return true;
-        },
         userAvailable : function(name, players)
         {
-            // players.foreach(function(p)
-            // {
-            //     if(name === p.roomName)
-            //     {
-            //         return false;
-            //     }
-            // });
-
             if(players[name] != null)
                 return false
             return true;
-        },
-
-
-        getOpenIndex : function(rooms)
-        {
-
         }
-
     };
